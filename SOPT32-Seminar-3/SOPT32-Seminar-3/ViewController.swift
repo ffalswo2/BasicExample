789972cba0a585afef927494d01f863f28a01772
Kim Minjae <ffalswo2@naver.com>
2023-04-22 14:25:03 +0900

    add: 세미나 3주차

---
+++ SOPT32-Seminar-3/SOPT32-Seminar-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT32-Seminar-3
+//
+//  Created by 김민재 on 2023/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

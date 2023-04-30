0eeed7db3c9e01504ae3bf5076f13fa719554c15
Kim Minjae <ffalswo2@naver.com>
2023-04-30 19:08:03 +0900

    add test study project

---
+++ UnitTestPractice/UnitTestPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestPractice
+//
+//  Created by 김민재 on 2023/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

fd6eb08f3953df4d33b749bfa71b45d9910cade3
MINJAE KIM <ffalswo2@naver.com>
2024-01-18 19:32:16 +0900

    init swiftui example

---
+++ SwiftUI-Ex/SwiftUI-Ex/SwiftUI_ExApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExApp.swift
+//  SwiftUI-Ex
+//
+//  Created by 김민재 on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
